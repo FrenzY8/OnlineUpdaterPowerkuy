0d78affbfdebba1577fe5dd0e6b5b724628bc347
Freeeeeeeeenzy <75056113+FrenzY8@users.noreply.github.com>
2024-07-22 16:12:10 +0700

    Update powerkuy.cpp

--- powerkuy.cpp
+++ powerkuy.cpp
@@ -2,8 +2,8 @@
 "Version": 7,
 "Creator": "PowerKuy",
 "Title": "About Recorder & Login Issue",
-"Info": "- For Now Please Dont Use Hack in Category(Recorder)\n- About Login Issue, Server cant handle too much player login at once, so just wait until you can login\n- This Announcement will end tommorow",
+"Info": "- No info",
 "Changelogs": "",
 "Link": "https://www.mediafire.com/file/uhg7otw466x5y6b/%255B_3.75_Auto_Port_%255D_Growtopia_Mod_Menu.apk/file",
-"Premium": false
+"Premium": true
 }
